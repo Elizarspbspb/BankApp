7915735a5615c5985aa2e48c8ebc8f6546657f76
elizar <elizar@abyss.wow>
2026-05-15 16:35:01 +0300

    Test commit

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ def index():
     # УЯЗВИМОСТЬ: прямой рендеринг пользовательского ввода (XSS)
     # http://127.0.0.1:5000/?name=<script>alert(1)%</script>
     template = f"<h1>Добро пожаловать {user_input}!</h1>"
+    template = f"<h1>Добро пожаловать {user_input}!</h1>"
     return render_template_string(template)
 
 if __name__ == '__main__':
