ab24d976a209019832f252ae53270b2055041aa1
elizar <elizar@abyss.wow>
2026-05-15 12:33:31 +0300

    Create file with XSS

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     user_input = request.args.get('name', '')
+    # УЯЗВИМОСТЬ: прямой рендеринг пользовательского ввода (XSS)
+    # http://127.0.0.1:5000/?name=<script>alert(1)%</script>
+    template = f"<h1>Добро пожаловать {user_input}!</h1>"
     return render_template_string(template)
 
 if __name__ == '__main__':
