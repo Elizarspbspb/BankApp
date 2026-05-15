75b194e84dd7cf5436fc3be2d44caf48c4dfcbe3
elizar <elizar@abyss.wow>
2026-05-15 15:31:25 +0300

    Defender xss

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, render_template_string
+from html import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     user_input = request.args.get('name', '')
-    # УЯЗВИМОСТЬ: прямой рендеринг пользовательского ввода (XSS)
+    # ИСПРАВЛЕНО: экранирование пользовательского ввода
     # http://127.0.0.1:5000/?name=<script>alert(1)%</script>
-    template = f"<h1>Добро пожаловать {user_input}!</h1>"
+    safe_input = escape(user_input)
+    template = f"<h1>Добро пожаловать {safe_input}!</h1>"
     return render_template_string(template)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)    # Отключаем debug в продакшене
